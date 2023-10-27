18704c7779554f476c0b8e56fced164796013c27
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-10-27 11:08:10 -0700

    Task 11

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,1 +1,24 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int i;
+	pid_t child_pid;
+
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid == 0)
+			exit(0);
+		else if (child_pid > 0)
+			printf("Zombie process created, PID: %d\n", child_pid);
+		else
+			perror("Fork failed");
+	}
+	while (1)
+		sleep(1);
+	return (0);
+}
+

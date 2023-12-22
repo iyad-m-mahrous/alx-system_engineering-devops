8f826a4a246214b880e44d6eb6d2ecab5e0df09a
iyad-m-mahrous <132349939+iyad-m-mahrous@users.noreply.github.com>
2023-12-22 17:30:51 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # Install install flask from pip3
-python::pip { 'Flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }

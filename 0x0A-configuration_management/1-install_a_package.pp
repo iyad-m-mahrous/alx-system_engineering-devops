f2d86d9c0e8e6f08ddb7764f4dc2f2913d341b17
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-12-22 06:00:08 -0800

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Using Puppet, install flask from pip3.
+# Install install flask from pip3
 package { 'flask':
-  provider => 'pip3',
-  version => '2.1.0',
+  ensure   => '2.1.0',
+  provider => 'pip',
 }

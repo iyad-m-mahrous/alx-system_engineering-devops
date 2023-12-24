d1a59ffb4a682370e494ab9e99c654ed1afdc481
iyad-m-mahrous <132349939+iyad-m-mahrous@users.noreply.github.com>
2023-12-24 11:15:26 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,9 +8,3 @@ python::pip { 'Flask':
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
-
-python::pip { 'Werkzeug':
-  ensure    => '2.1.0',
-  provider  => 'pip3',
-  require   => Package['python3-pip'],
-}
